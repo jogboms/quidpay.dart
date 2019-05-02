091a019090d132291e6987e49b3d6f3f9f9b043c
Jogboms <jeremiahogbomo@gmail.com>
2019-05-02 22:30:15 +0200

    test(example): Validate

--- example/validate.dart
+++ example/validate.dart
@@ -5,22 +5,25 @@ import 'charge.dart' as charge;
 
 void card() async {
   final response = await charge.pin();
-  await Validate().card(
+  final resp = await Validate().card(
     otp: '12345',
     flwRef: response.data.flwRef,
   );
+  print(resp);
 }
 
 void account() async {
   final response = await charge.account();
-  await Validate().account(
+  final resp = await Validate().account(
     otp: '12345',
     flwRef: response.data.flwRef,
   );
+  print(resp);
 }
 
 void main() async {
   Quidpay.init(production: false, publicKey: PUBK, secretKey: SECK);
 
   card();
+  account();
 }
