44864726565b75dc4ce69a156a526e7f43fecb8e
Jogboms <jeremiahogbomo@gmail.com>
2021-06-03 10:00:41 +0200

    Fix test analysis

--- test/_constants.dart
+++ test/_constants.dart
@@ -3,5 +3,4 @@ const SECK_TEST = 'FLWSECK-b42f2eacd6b92bda2f560c7e85b41cfd-X';
 const RAW_DATA = <String, int>{'a': 1, 'b': 2, 'c': 3};
 const ENCRYPTED_DATA = 'Z6L8BhZ9rcTku0YJiaYa6+eyH0yAEaar';
 const GENERATED_KEY = 'bb9714020722eb4cf7a169f2';
-const GENERATED_INTEGRITY_HASH =
-    '01a9467aee23fa8988a0968bcf04b534c8a0c67ee97e2bcd61c0e90e72cf1699';
+const GENERATED_INTEGRITY_HASH = '01a9467aee23fa8988a0968bcf04b534c8a0c67ee97e2bcd61c0e90e72cf1699';
